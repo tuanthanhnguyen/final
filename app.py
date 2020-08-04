784ed5c24640b594d1cdfcf82fa6e04c4e8c3cc2
tuanthanhnguyen <63715128+tuanthanhnguyen@users.noreply.github.com>
2020-08-04 09:09:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,5 @@
-'''from flask import Flask, render_template,send_file
 
-app = Flask(__name__)
-
-
-
-
-
-@app.route("/")
-
-def home():
-
-    return render_template('home.html')
-
-
-
-
-
-if __name__ == '__main__':
-
-    app.run(debug=True)'''
-from flask import Flask, render_template 
+from flask import Flask, render_template ,send_file
  
 app = Flask(__name__) 
  
